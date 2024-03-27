64606986d2c791e2f40a4cbba31dd078912bf0c5
Daniel Vinciguerra <daniel.vinciguerra@bivee.com.br>
2024-03-27 14:50:21 -0300

    chore: disable rspec focus on CI env

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,7 +11,7 @@ RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
 
-  config.filter_run_when_matching :focus
+  config.filter_run_when_matching :focus unless ENV["CI"]
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
